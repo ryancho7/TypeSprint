1496abc846db3d7b87de56fae5a3ae19c8b42d70
Ryan Cho <ryancho7@uw.edu>
2025-05-19 15:40:07 -0700

    working race functionality - local/solo

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.js';
 import Dashboard from './pages/Dashboard.js';
+import Race from './pages/Race.js';
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/race" element={<Race />} />
         </Routes>
     </Router>
   );
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,13 +1,26 @@
+import { useNavigate } from "react-router-dom"
+
 export default function Dashboard() {
+
+    const navigate = useNavigate();
+
     return (
         <div className="flex flex-col gap-[200px] justify-center items-center bg-[#1E1E1E] min-h-screen">
             <h1 className="bg-gradient-to-b from-slate-50 to-neutral-500 bg-clip-text text-transparent font-bold text-[228.47px] ">Type Sprint</h1>
-            <a
-                className="border px-[213px] py-2 rounded-[40px] text-white transition duration-300 ease-in-out hover:text-[#1E1E1E] hover:bg-white"
-                href='/signout'
-            >
-                Sign Out
-            </a>
+            <div>
+                <a
+                    className="border px-[213px] py-2 rounded-[40px] text-white transition duration-300 ease-in-out hover:text-[#1E1E1E] hover:bg-white"
+                    href='/signout'
+                >
+                    Sign Out
+                </a>
+                <button 
+                    className="border px-[213px] py-2 rounded-[40px] text-white transition duration-300 ease-in-out hover:text-[#1E1E1E] hover:bg-white"
+                    onClick={() => navigate('/Race')}
+                >
+                    Go To Race
+                </button>
+            </div>
         </div>
     )
 }
