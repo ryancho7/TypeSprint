7f97cd2950a37c4f98a65001d9179d0018fcacbd
yl0517 <yl0517@uw.edu>
2025-05-21 00:08:31 -0700

    dividing things up into components, using react contexts for auth instead of prop spamming, follow up changes after context auth

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,25 +1,19 @@
-import { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
+import Title from '../components/Title.js'
+import { useContext } from 'react'
+import { AuthContext } from '../contexts/authContext.js'
 
 export default function Home() {
-    const [auth, setAuth] = useState({ isAuthenticated: false })
     const navigate = useNavigate()
 
-    useEffect(() => {
-        fetch('/api/auth')
-            .then(res => res.json())
-            .then(data => setAuth({ ...data }))
-            .catch(() => setAuth({ isAuthenticated: false }))
-    }, [])
+    const { auth } = useContext(AuthContext)
 
     // not signed in
     if (!auth.isAuthenticated) {
         console.log("not signed in")
         return (
             <div className="flex flex-col gap-52 justify-center items-center bg-[black] min-h-screen">
-                <h1 className="bg-gradient-to-b from-slate-50 to-neutral-500 bg-clip-text text-transparent font-bold text-[228px]">
-                    Type Sprint
-                </h1>
+                <Title />
                 <div className="flex space-x-4">
                     <a
                         className="border px-52 py-2 rounded-[40px] text-white transition hover:text-[#1E1E1E] hover:bg-white"
@@ -42,9 +36,7 @@ export default function Home() {
     console.log("signed in")
     return (
         <div className="flex flex-col gap-52 justify-center items-center bg-[black] min-h-screen">
-            <h1 className="bg-gradient-to-b from-slate-50 to-neutral-500 bg-clip-text text-transparent font-bold text-[228px]">
-                Type Sprint
-            </h1>
+            <Title />
             <div className="flex space-x-4">
                 <a
                     className="border px-32 py-2 rounded-[40px] text-white transition hover:text-[#1E1E1E] hover:bg-white"
