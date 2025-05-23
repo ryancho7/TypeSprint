684cdb11407b5b23f6ddcfd373af13aed44d1a0b
yl0517 <yl0517@uw.edu>
2025-05-23 00:21:22 -0700

    ws implemented with socket.io, working with 1 player

--- server/app.js
+++ server/app.js
@@ -89,7 +89,8 @@ app.use('/*', createProxyMiddleware({
     // target: 'http://127.0.0.1:4000',
     target: 'http://localhost:4000',
     pathRewrite: (path, req) => req.baseUrl,
-    changeOrigin: true
+    changeOrigin: true,
+    ws: true
 }))
 
 export default app;
---
+++ server/websocket/server.js
@@ -0,0 +1,49 @@
+import app from '../app.js';
+import http from 'http';
+import { Server } from 'socket.io';
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: { origin: 'http://localhost:4000', credentials: true }
+});
+
+// simple in-memory game state
+const races = {}; // { raceId: { text, participants: { socketId: progress } } }
+
+io.on('connection', (socket) => {
+    console.log('client connected:', socket.id);
+
+    socket.on('joinRace', ({ raceId }) => {
+        socket.join(raceId);
+        races[raceId] ||= { text: "temp temp temp temp a b c d", participants: {} };
+        races[raceId].participants[socket.id] = 0;
+        io.in(raceId).emit('raceState', races[raceId]);
+    });
+
+    socket.on('updateProgress', ({ raceId, progress }) => {
+        if (!races[raceId]) return;
+        races[raceId].participants[socket.id] = progress;
+        io.in(raceId).emit('progressUpdate', {
+            participants: races[raceId].participants
+        });
+    });
+
+    socket.on('startRace', ({ raceId }) => {
+        io.in(raceId).emit('start', { text: races[raceId].text });
+    });
+
+    socket.on('disconnecting', () => {
+        for (const room of socket.rooms) {
+            if (races[room]?.participants) {
+                delete races[room].participants[socket.id];
+                io.in(room).emit('progressUpdate', {
+                    participants: races[room].participants
+                });
+            }
+        }
+    });
+});
+
+const PORT = 3000;
+server.listen(PORT, () => console.log(`listening on ${PORT}`));
