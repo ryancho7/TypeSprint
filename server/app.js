e53e8f4d2c833db376afdd7ee961425806bde24e
ryancho7 <114904773+ryancho7@users.noreply.github.com>
2025-05-23 22:33:40 -0700

    prep for deploy

--- server/app.js
+++ server/app.js
@@ -17,7 +17,8 @@ const authConfig = {
         clientId: process.env.AZURE_CLIENT_ID,
         authority: `https://login.microsoftonline.com/${process.env.AZURE_TENANT_ID}`,
         clientSecret: process.env.AZURE_CLIENT_SECRET,
-        redirectUri: "/redirect"
+        // redirectUri: "/redirect"
+        redirectUri: "https://typesprint.ryancho.me/redirect"
     },
     system: {
         loggerOptions: {
@@ -79,8 +80,8 @@ app.use((req, res, next) => {
 // PROXY
 app.use('/*', createProxyMiddleware({
     // for windows
-    target: 'http://127.0.0.1:4000',
-    // target: 'http://localhost:4000',
+    // target: 'http://127.0.0.1:4000',
+    target: 'http://localhost:4000',
     pathRewrite: (path, req) => req.baseUrl,
     changeOrigin: true,
     ws: true
